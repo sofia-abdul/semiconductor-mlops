1e3f690068be8043566581d06779f0b24c382712
Sofia Abdul <sofiaabdul721@gmail.com>
2026-04-26 12:29:33 +0100

    Fix database engine setup for pipeline execution

--- pipeline/db.py
+++ pipeline/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
+
 from pipeline.config import DB_URL
 
 
 def get_engine():
-     """Create and return a SQLAlchemy database engine."""
     return create_engine(DB_URL)
